14cbf13e271804611b07d3722c0b4ed6d5cb6b53
takuoka <blackn.red42@gmail.com>
2021-04-27 22:14:27 +0900

    Minimum Adjust

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,29 +8,31 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <img
+        {/* <img
           src="/vercel-and-notion.png"
           height="85"
           width="250"
           alt="Vercel + Notion"
-        />
+        /> */}
         <h1>My Notion Blog</h1>
         <h2>
-          Blazing Fast Notion Blog with Next.js'{' '}
+          This is my notion blog.
+          {/* Blazing Fast Notion Blog with Next.js'{' '}
           <ExtLink
             href="https://github.com/vercel/next.js/issues/9524"
             className="dotted"
             style={{ color: 'inherit' }}
           >
             SSG
-          </ExtLink>
+          </ExtLink> */}
         </h2>
 
-        <Features />
+        {/* <Features /> */}
 
         <div className="explanation">
           <p>
-            This is a statically generated{' '}
+            Hello, world.
+            {/* This is a statically generated{' '}
             <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
             <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
             is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>
@@ -44,11 +46,11 @@ export default function Index() {
             </ExtLink>{' '}
             which allow us to achieve all of the benefits listed above including
             blazing fast speeds, great local editing experience, and always
-            being available!
+            being available! */}
           </p>
 
           <p>
-            Get started by creating a new page in Notion and clicking the deploy
+            {/* Get started by creating a new page in Notion and clicking the deploy
             button below. After you supply your token and the blog index id (the
             page's id in Notion) we will automatically create the table for you!
             See{' '}
@@ -58,7 +60,7 @@ export default function Index() {
             for finding the new page's id. To get your token from Notion, login
             and look for a cookie under www.notion.so with the name `token_v2`.
             After finding your token and your blog's page id you should be good
-            to go!
+            to go! */}
           </p>
         </div>
       </div>
